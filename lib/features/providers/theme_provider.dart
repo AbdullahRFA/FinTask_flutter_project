5929b0da626222d5a09028a8a148c3deeaa0c08e
AbdullahRFA <shakibrybmn@gmail.com>
2026-01-12 21:57:15 +0600

    Set default theme to Dark Mode
    Updated themeProvider initialization value to true so the app starts in dark mode by default.
    
    Signed-off-by: AbdullahRFA <shakibrybmn@gmail.com>

--- lib/features/providers/theme_provider.dart
+++ lib/features/providers/theme_provider.dart
@@ -2,4 +2,4 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 // False = Light Mode, True = Dark Mode
-final themeProvider = StateProvider<bool>((ref) => false);
+final themeProvider = StateProvider<bool>((ref) => true);
